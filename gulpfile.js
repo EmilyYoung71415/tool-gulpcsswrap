f4ea7039f61a9c87ccb8f9d509d072094d7d8862
yiwang <18842613507@163.com>
2018-06-26 11:00:38 +0800

    add cli FILENAME add gitignore add gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@ var path = require('path')
 var gulp = require('gulp')
 var cleanCSS = require('gulp-clean-css')
 var cssWrap = require('gulp-css-wrap')
+
+var filename = process.env.FILENAME || '1b1e24'
+
 gulp.task('css-wrap', function () {
 	return gulp.src(path.resolve('./theme/index.css'))/* 找需要添加命名空间的css文件，支持正则表达式 */
 		.pipe(cssWrap({
-			selector: '.custom-1b1e24' /* 添加的命名空间 */
+			selector: `.custom-${filename}` /* 添加的命名空间 */
 		}))
 		.pipe(cleanCSS())
-		.pipe(gulp.dest('assets/theme/1b1e24')) /* 存放的目录 */
+		.pipe(gulp.dest(`assets/theme/${filename}`)) /* 存放的目录 */
 })
+
+gulp.task('default', ['css-wrap'])
